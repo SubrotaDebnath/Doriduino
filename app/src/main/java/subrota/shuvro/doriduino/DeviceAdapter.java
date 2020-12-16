d7d3cdc05ed767cb619f74b70d227318d7be5b0d
Subrota Debnath <subrotadebnath1807@gmail.com>
2020-12-16 21:18:33 +0600

    Device list Adapter

---
+++ app/src/main/java/subrota/shuvro/doriduino/DeviceAdapter.java
@@ -0,0 +1,70 @@
+package subrota.shuvro.doriduino;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder>{
+    private List<DeviceInfoDataSet> devices = new ArrayList<>();
+    private  Set<BluetoothDevice> pairedDevices;
+    private Context context;
+
+    public DeviceAdapter(Context context, List<DeviceInfoDataSet> devices, Set<BluetoothDevice> pairedDevices) {
+        this.devices = devices;
+        this.context = context;
+        this.pairedDevices = pairedDevices;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.device_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.name.setText(devices.get(position).getName());
+        holder.id.setText(devices.get(position).getId());
+        holder.deviceRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ControlBoard.class);
+                intent.putExtra("id", devices.get(position).getId());
+                intent.putExtra("name", devices.get(position).getName());
+                intent.putExtra("devices", (Serializable) pairedDevices);
+                //ContextCompat.startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return devices.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name, id;
+        LinearLayout deviceRow;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.device_name);
+            id = itemView.findViewById(R.id.device_id);
+            deviceRow = itemView.findViewById(R.id.deviceRow);
+        }
+    }
+}
--- app/src/main/java/subrota/shuvro/doriduino/DeviceList.java
+++ app/src/main/java/subrota/shuvro/doriduino/DeviceList.java
@@ -1,10 +1,16 @@
 package subrota.shuvro.doriduino;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,5 +27,28 @@ public class DeviceList extends AppCompatActivity {
         setContentView(R.layout.activity_device_list);
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceAddress = device.getAddress(); // MAC address
+                DeviceListDataSet deviceInfoModel = new DeviceListDataSet(deviceName,deviceAddress);
+                deviceList.add(deviceInfoModel);
+            }
+            RecyclerView recyclerView = findViewById(R.id.paired_devices);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            DeviceListAdapter deviceListAdapter = new DeviceListAdapter(this,deviceList);
+            recyclerView.setAdapter(deviceListAdapter);
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+        } else {
+            View view = findViewById(R.id.recyclerViewDevice);
+            Snackbar snackbar = Snackbar.make(view, "Activate Bluetooth or pair a Bluetooth device", Snackbar.LENGTH_INDEFINITE);
+            snackbar.setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) { }
+            });
+            snackbar.show();
+        }
     }
+
 }
