7ce259f2169f320a7dcf9bbf2a195fc8316c8f7a
Subrota Debnath <subrotadebnath1807@gmail.com>
2020-12-16 21:14:17 +0600

    Device list dataSet

--- app/src/main/java/subrota/shuvro/doriduino/DeviceListDataSet.java
+++ app/src/main/java/subrota/shuvro/doriduino/DeviceListDataSet.java
@@ -1,4 +1,27 @@
 package subrota.shuvro.doriduino;
 
 public class DeviceListDataSet {
+    private String name;
+    private String address;
+
+    public DeviceListDataSet(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
